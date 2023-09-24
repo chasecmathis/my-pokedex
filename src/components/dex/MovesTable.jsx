bd544d6fa3b78c930b637eb4eb20550a9a2c5b01
Chase Mathis <chase.mathis313@gmail.com>
2023-09-24 15:12:36 -0500

    Display more move data

--- src/components/dex/MovesTable.jsx
+++ src/components/dex/MovesTable.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   TableContainer,
   Table,
@@ -7,17 +8,60 @@ import {
   TableBody,
   Paper,
   Chip,
-  Typography
+  Typography,
+  Box,
+  FormControl,
+  MenuItem,
+  InputLabel,
+  Select
 } from "@mui/material";
+import React, { useState, useEffect } from "react";
 
 const moves = require("@/data/moves.json");
 
 export default function MovesTable({ pkmn }) {
+  const [version, setVersion] = useState(pkmn.version_groups[0]);
+
+  function handleChange(event) {
+    setVersion(event.target.value);
+  }
+
+  function getAllMoves(moves) {
+    const allMoves = [];
+    moves.forEach((move) =>
+      move.learn_details
+        .filter((m) => m.game === version)
+        .forEach((m) => allMoves.push({ name: move.name, level: m.level }))
+    );
+
+    return allMoves.sort((a, b) => a.level - b.level);
+  }
+
   return (
     <>
-      <Typography variant="h5" fontWeight="500" paddingY={2}>
-        Level-Up Moves
-      </Typography>
+      <Box display="flex" alignItems="center" gap={4}>
+        <Typography variant="h5" fontWeight="500" paddingY={2}>
+          Level-Up Moves
+        </Typography>
+        <FormControl sx={{ m: 1, minWidth: 120, textTransform: "capitalize" }} size="small">
+          <InputLabel id="select-version-label">Version</InputLabel>
+          <Select
+            labelId="select-version-label"
+            id="select-version-label"
+            value={version}
+            label="Version"
+            onChange={handleChange}
+            MenuProps={{ PaperProps: { sx: { maxHeight: "25%" } } }}
+          >
+            {pkmn.version_groups.map((ver) => (
+              <MenuItem key={ver} value={ver} sx={{textTransform: "capitalize" }}>
+                {ver}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+
       <TableContainer variant="outlined" component={Paper}>
         <Table size="small" aria-label="moves table">
           <TableHead sx={{ bgcolor: `${pkmn.types[0]}` + ".light" }}>
@@ -32,52 +76,40 @@ export default function MovesTable({ pkmn }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {pkmn.moves
-              .sort((a, b) => a.level - b.level)
-              .map((move) => {
-                const moveData = moves.find((data) => data.name === move.name);
-                return (
-                  <TableRow key={move.name}>
-                    <TableCell align="right">{move.level}</TableCell>
-                    <TableCell
-                      align="left"
-                      sx={{ textTransform: "capitalize" }}
-                    >
-                      {move.name.replace("-", " ")}
-                    </TableCell>
-                    <TableCell
-                      align="left"
-                      sx={{ textTransform: "capitalize" }}
-                    >
-                      <Chip
-                        size="small"
-                        label={moveData.type}
-                        variant="filled"
-                        sx={{
-                          bgcolor: `${moveData.type}` + ".main",
-                          color: "white",
-                          border: 2,
-                          borderColor: `${moveData.type}` + ".light",
-                          fontWeight: 500
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell
-                      align="left"
-                      sx={{ textTransform: "capitalize" }}
-                    >
-                      {moveData.category}
-                    </TableCell>
-                    <TableCell align="right">
-                      {moveData.power || "- -"}
-                    </TableCell>
-                    <TableCell align="right">
-                      {moveData.accuracy || "- -"}
-                    </TableCell>
-                    <TableCell align="left">{moveData.description}</TableCell>
-                  </TableRow>
-                );
-              })}
+            {getAllMoves(pkmn.moves).map((move) => {
+              console.log(move);
+              const moveData = moves.find((data) => data.name === move.name);
+              return (
+                <TableRow key={move.name + move.level}>
+                  <TableCell align="right">{move.level}</TableCell>
+                  <TableCell align="left" sx={{ textTransform: "capitalize" }}>
+                    {move.name.replace("-", " ")}
+                  </TableCell>
+                  <TableCell align="left" sx={{ textTransform: "capitalize" }}>
+                    <Chip
+                      size="small"
+                      label={moveData.type}
+                      variant="filled"
+                      sx={{
+                        bgcolor: `${moveData.type}` + ".main",
+                        color: "white",
+                        border: 2,
+                        borderColor: `${moveData.type}` + ".light",
+                        fontWeight: 500,
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell align="left" sx={{ textTransform: "capitalize" }}>
+                    {moveData.category}
+                  </TableCell>
+                  <TableCell align="right">{moveData.power || "- -"}</TableCell>
+                  <TableCell align="right">
+                    {moveData.accuracy || "- -"}
+                  </TableCell>
+                  <TableCell align="left">{moveData.description}</TableCell>
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
--- src/data/store-data.js
+++ src/data/store-data.js
@@ -1,89 +1,93 @@
 const fs = require("fs");
 
-// Function to fetch the list of all Pokémon species
-async function fetchAllSpecies() {
-  const url = "https://pokeapi.co/api/v2/pokemon-species?limit=100000&offset=0";
+async function fetchJSON(url) {
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    return data.results;
+    return await response.json();
   } catch (error) {
-    console.error("Failed to fetch the list of Pokémon species");
-    return [];
-  }
-}
-
-// Function to fetch default Pokémon data for a given species URL
-async function fetchDefaultPokemonData(speciesUrl) {
-  try {
-    const response = await fetch(speciesUrl);
-    const speciesData = await response.json();
-    const defaultPokemonUrl = speciesData.varieties.find(
-      (variety) => variety.is_default
-    ).pokemon.url;
-    const defaultPokemonResponse = await fetch(defaultPokemonUrl);
-    const defaultPokemonData = await defaultPokemonResponse.json();
-    return {
-      ...defaultPokemonData,
-      ...speciesData,
-    };
-  } catch (error) {
-    console.error(
-      `Failed to fetch default Pokémon data for species URL: ${speciesUrl}`
-    );
+    console.error(`Failed to fetch data from URL: ${url}`);
     return null;
   }
 }
 
-// Fetch and store data for all unique Pokémon species
 async function fetchAllPokemon() {
   const allPokemon = [];
 
-  const speciesList = await fetchAllSpecies();
-  for (const species of speciesList) {
-    const pokemonData = await fetchDefaultPokemonData(species.url);
-    if (pokemonData) {
-      delete pokemonData.sprites.versions;
-      const pokemonInfo = {
-        name: pokemonData.name,
-        id: pokemonData.id,
-        types: pokemonData.types.map((typeData) => typeData.type.name),
-        stats: pokemonData.stats.map((stat) => ({
-          base_stat: stat.base_stat,
-          name: stat.stat.name,
-        })),
-        abilities: pokemonData.abilities.map(
-          (abilityData) => abilityData.ability.name
-        ),
-        moves: pokemonData.moves
-          .filter((move) =>
-            move.version_group_details.some(
-              (move) => move.move_learn_method.name === "level-up"
-            )
-          )
-          .map((moveData) => ({
-            name: moveData.move.name,
-            level: moveData.version_group_details.findLast(
-              (move) => move.move_learn_method.name === "level-up"
-            ).level_learned_at,
-          })),
-        sprites: pokemonData.sprites,
-        // You can handle description, evolutions, generation, etc.
-        description:
-          pokemonData.flavor_text_entries.length !== 0
-            ? pokemonData.flavor_text_entries.find(
-                (desc) => desc.language.name === "en"
-              ).flavor_text.replace('\f', ' ')
-            : null,
-        generation: pokemonData.generation.name,
-        evolves_from: pokemonData.evolves_from_species
-          ? pokemonData.evolves_from_species.name
-          : null,
-      };
+  const speciesList = await fetchJSON(
+    "https://pokeapi.co/api/v2/pokemon-species?limit=100000&offset=0"
+  );
+  if (!speciesList) return;
 
-      console.log(pokemonInfo.name)
-      allPokemon.push(pokemonInfo);
-    }
+  for (const species of speciesList.results) {
+    const speciesData = await fetchJSON(species.url);
+    if (!speciesData) continue;
+
+    const defaultPokemonUrl = speciesData.varieties.find(
+      (variety) => variety.is_default
+    ).pokemon.url;
+    const defaultPokemonData = await fetchJSON(defaultPokemonUrl);
+    if (!defaultPokemonData) continue;
+
+    // Pokemon Info
+    const pokemonInfo = {
+      name: defaultPokemonData.name,
+      id: defaultPokemonData.id,
+      types: defaultPokemonData.types.map((typeData) => typeData.type.name),
+      stats: defaultPokemonData.stats.map((stat) => ({
+        base_stat: stat.base_stat,
+        name: stat.stat.name,
+      })),
+      abilities: defaultPokemonData.abilities.map(
+        (abilityData) => abilityData.ability.name
+      ),
+      moves: [], // Placeholder for moves
+      version_groups: [], // Placeholder for version groups
+      sprites: defaultPokemonData.sprites,
+      description:
+        speciesData.flavor_text_entries.length !== 0
+          ? speciesData.flavor_text_entries
+              .find((desc) => desc.language.name === "en")
+              .flavor_text.replace("\f", " ")
+          : null,
+      generation: speciesData.generation.name,
+      evolves_from: speciesData.evolves_from_species
+        ? speciesData.evolves_from_species.name
+        : null,
+    };
+
+    // Fetch and store level up moves
+
+    const levelUpMoves = defaultPokemonData.moves.filter((move) =>
+      move.version_group_details.some(
+        (move) => move.move_learn_method.name === "level-up"
+      )
+    );
+
+    const moves = levelUpMoves.map((moveData) => {
+      const moveLearnDetails = moveData.version_group_details
+        .filter((move) => move.move_learn_method.name === "level-up")
+        .map((move) => ({
+          game: move.version_group.name,
+          level: move.level_learned_at,
+        }));
+      return {
+        name: moveData.move.name,
+        learn_details: moveLearnDetails,
+      };
+    });
+
+    // Create an array to store unique version groups
+    const versionGroups = [
+      ...new Set(
+        moves.flatMap((move) => move.learn_details.map((detail) => detail.game))
+      ),
+    ];
+
+    pokemonInfo.moves = moves;
+    pokemonInfo.version_groups = versionGroups;
+
+    console.log(pokemonInfo.name);
+    allPokemon.push(pokemonInfo);
   }
 
   fs.writeFileSync("pokemon.json", JSON.stringify(allPokemon, null, 4));
@@ -91,69 +95,45 @@ async function fetchAllPokemon() {
 }
 
 async function fetchTypeData(typeUrl) {
-  try {
-    const response = await fetch(typeUrl);
-    const typeData = await response.json();
-    return typeData;
-  } catch (e) {
-    console.error(
-      `Failed to fetch default Pokémon data for type URL: ${url}`,
-      e
-    );
-    return null;
-  }
+  return await fetchJSON(typeUrl);
 }
 
-// Fetch all type data
 async function fetchTypes() {
-  const url = "https://pokeapi.co/api/v2/type";
-  try {
-    const response = await fetch(url);
-    const typeData = await response.json();
-    return typeData.results.filter(
-      (type) => type.name !== "shadow" && type.name !== "unknown"
-    );
-  } catch (e) {
-    console.error(
-      `Failed to fetch default Pokémon data for type URL: ${url}`,
-      e
-    );
-    return [];
-  }
+  const typeData = await fetchJSON("https://pokeapi.co/api/v2/type");
+  if (!typeData) return [];
+
+  return typeData.results.filter(
+    (type) => type.name !== "shadow" && type.name !== "unknown"
+  );
 }
 
 async function fetchAllTypes() {
   const types = await fetchTypes();
   const typeInfo = [];
+
   for (const type of types) {
     const typeData = await fetchTypeData(type.url);
-    if (typeData) {
-      const type = {
-        name: typeData.name,
-        damage_relations: typeData.damage_relations,
-      };
+    if (!typeData) continue;
 
-      typeInfo.push(type);
-    }
+    const typeInfoObj = {
+      name: typeData.name,
+      damage_relations: typeData.damage_relations,
+    };
+
+    typeInfo.push(typeInfoObj);
   }
+
   fs.writeFileSync("types.json", JSON.stringify(typeInfo, null, 4));
   console.log("Finished fetching types");
 }
 
-// Fetch all generation data
 async function fetchGenerations() {
-  const url = "https://pokeapi.co/api/v2/generation";
-  try {
-    const response = await fetch(url);
-    const generationData = await response.json();
-    return generationData.results.map((generation) => generation.name);
-  } catch (e) {
-    console.error(
-      `Failed to fetch default Pokémon data for generation URL: ${url}`,
-      e
-    );
-    return [];
-  }
+  const generationData = await fetchJSON(
+    "https://pokeapi.co/api/v2/generation"
+  );
+  if (!generationData) return [];
+
+  return generationData.results.map((generation) => generation.name);
 }
 
 async function fetchAllGenerations() {
@@ -163,67 +143,53 @@ async function fetchAllGenerations() {
 }
 
 async function fetchMoveData(moveUrl) {
-  try {
-    const response = await fetch(moveUrl);
-    const moveData = await response.json();
-    return moveData;
-  } catch (e) {
-    console.error(
-      `Failed to fetch default Pokémon data for move URL: ${url}`,
-      e
-    );
-    return null;
-  }
+  return await fetchJSON(moveUrl);
 }
 
-// Fetch all type data
 async function fetchMoves() {
-  const url = "https://pokeapi.co/api/v2/move?limit=100000&offset=0";
-  try {
-    const response = await fetch(url);
-    const moveData = await response.json();
-    return moveData.results;
-  } catch (e) {
-    console.error(
-      `Failed to fetch default Pokémon data for move URL: ${url}`,
-      e
-    );
-    return [];
-  }
+  const moveData = await fetchJSON(
+    "https://pokeapi.co/api/v2/move?limit=100000&offset=0"
+  );
+  if (!moveData) return [];
+
+  return moveData.results;
 }
 
 async function fetchAllMoves() {
   const moves = await fetchMoves();
   const moveInfo = [];
+
   for (const move of moves) {
     const moveData = await fetchMoveData(move.url);
-    if (moveData && moveData.type.name !== 'shadow') {
-      const descripion = moveData.effect_entries.find(
-        (entry) => entry.language.name === "en"
-      );
-      const move = {
-        name: moveData.name,
-        accuracy: moveData.accuracy,
-        power: moveData.power,
-        type: moveData.type.name,
-        category: moveData.damage_class.name,
-        description: descripion
-          ? descripion.short_effect.replace("$effect_chance% ", "")
-          : "No description",
-      };
+    if (!moveData || moveData.type.name === "shadow") continue;
 
-      moveInfo.push(move);
-    }
+    const description = moveData.effect_entries.find(
+      (entry) => entry.language.name === "en"
+    );
+
+    const moveInfoObj = {
+      name: moveData.name,
+      accuracy: moveData.accuracy,
+      power: moveData.power,
+      type: moveData.type.name,
+      category: moveData.damage_class.name,
+      description: description
+        ? description.short_effect.replace("$effect_chance% ", "")
+        : "No description",
+    };
+
+    moveInfo.push(moveInfoObj);
   }
+
   fs.writeFileSync("moves.json", JSON.stringify(moveInfo, null, 4));
   console.log("Finished fetching moves");
 }
 
-function fetchData() {
-  fetchAllMoves();
-  fetchAllPokemon();
-  fetchAllTypes();
-  fetchAllGenerations();
+async function fetchData() {
+  // await fetchAllMoves();
+  await fetchAllPokemon();
+  await fetchAllTypes();
+  await fetchAllGenerations();
 }
 
 fetchData();
